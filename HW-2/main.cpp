f6665a3048c1dd9fca3c150c508cb4230e3185ff
Petr Peresadko <senior.peresadko2016@yandex.ru>
2021-03-06 13:02:10 +0300

    improved

--- HW-2/main.cpp
+++ HW-2/main.cpp
@@ -40,20 +40,33 @@ public:
         this->m = this->m / l;
         return *this;
     }
+
     Drob &operator += (const Drob &another){
-        this->n = another.m * this->n + another.n * this->m;
-        this->m = this->m * another.n;
-        int l = lcm(this->m, this->n);
+        int s = gcd(this->m, another.m);
+        this->n = this->n * s / this->m + another.n * s / another.m;
+        this->m = s;
+        int l = lcm(this->n, this->m);
         this->n = this->n / l;
         this->m = this->m / l;
+        if (this->m < 1)
+        {
+            this->m = -1 * this->m;
+            this->n = -1 * this->n;
+        }
         return *this;
     }
     Drob &operator -= (const Drob &another){
-        this->n = another.m * this->n - another.n * this->m;
-        this->m = this->m * another.n;
-        int l = lcm(this->m, this->n);
+        int s = gcd(this->m, another.m);
+        this->n = this->n * s / this->m - another.n * s / another.m;
+        this->m = s;
+        int l = lcm(this->n, this->m);
         this->n = this->n / l;
         this->m = this->m / l;
+        if (this->m < 1)
+        {
+            this->m = -1 * this->m;
+            this->n = -1 * this->n;
+        }
         return *this;
     }
     Drob &operator *= (const Drob &another){
@@ -73,38 +86,17 @@ public:
         return *this;
     }
 
-//    Drob same(){
-//        bool flag = true;
-//        if(m == n){
-//            n = 1;
-//            m = 1;
-//        }
-//
-//        else{
-//            int temp = 0;
-//            temp = std::min(m, n);
-//            for(int i = 2; temp; i++){
-//                if(n % i == 0 && m % i == 0){
-//                    n = n / i;
-//                    m = m / i;
-//                }
-//            }
-//        }
-//    }
+    int gcd(int a, int b) {
+        auto x = a;
+        auto y = b;
+        while (a && b)
+            if (a > b) a %= b;
+            else b %= a;
 
-    int gcd(int n, int m) {
-        int x = 0;
-        x = n;
-        int y = 0;
-        y = m;
-        while (n && m) {
-            if (n > m) n %= m;
-            else m %= n;
-        }
-        return (x * y) / (n + m);
+        return (x * y) / (a + b);
     }
-    int lcm(int n, int m) {
-        return (n * m) / gcd(n, m);
+    int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
     }
 
 };
@@ -112,9 +104,14 @@ public:
 int main() {
     Drob a(10, 5);
     Drob b(1, 2);
-    a += b;
     b.Print();
     a.Print();
+    a += b;
+    a.Print();
+    a *= b;
+    a.Print();
+    Drob c(a);
+    c.Print();
 
     return 0;
 }
